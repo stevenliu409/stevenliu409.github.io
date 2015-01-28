9eee97c38e6e663e0aa363b7929fdf1eb6bb5cbd
Steven <steven.github@gmail.com>
2015-01-28 15:44:10 -0800

    fixed animation bug

--- animation.js
+++ animation.js
@@ -12,18 +12,40 @@ $(document).ready(function() {
     });
 
     $("#back-to-top a").click(function(event) {
-      $('html, body').animate({scrollTop:0}, 1500);
+      stopScroll();
+
+      $('html, body').animate({scrollTop:0}, 1500, continueScroll);
 
       // This prevents the default action of the anchor tag to be called.
       //  Can be interchanged with event.preventDefault()
       return false;
+
     });
 
     $("#navbar a").click(function(event) {
+      stopScroll();
+
+      // else do the scroll animation
       var link = $(event.target).attr("href");
       var jumpToTag = $(link);
-      $('html,body').animate({scrollTop: jumpToTag.offset().top},800);
+
+      $('html,body').animate({scrollTop: jumpToTag.offset().top}, 800, continueScroll);
       return false;
     });
   });
 });
+
+
+
+// Helper Functions
+// Taken from http://stackoverflow.com/a/18445654
+function stopScroll() {
+  // if user scrolls while animation, cancel the animation
+  $("html, body").bind("scroll mousedown DOMMouseScroll mousewheel keyup", function(){
+    $('html, body').stop();
+  });
+}
+
+function continueScroll() {
+  $("html, body").unbind("scroll mousedown DOMMouseScroll mousewheel keyup");
+}
