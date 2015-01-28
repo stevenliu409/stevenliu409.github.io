23f231cc5cd60e0563c4ad89400ef8ce3afe1fda
Steven <steven.github@gmail.com>
2015-01-28 06:17:52 -0800

    fixed back to top button bug

--- animation.js
+++ animation.js
@@ -8,16 +8,21 @@ $(document).ready(function() {
   $("#back-to-top").hide();
 
   $(function () {
-      $(window).scroll(function () {
-        if ($(this).scrollTop() > 100) {
-          $('#back-to-top').fadeIn();
-        } else {
-          $('#back-to-top').fadeOut();
-        }
-      });
+    $(window).scroll(function () {
+      if ($(this).scrollTop() > 100) {
+        $('#back-to-top').fadeIn();
+      } else {
+        $('#back-to-top').fadeOut();
+      }
+    });
 
-  $("#back-to-top a").click(function(event) {
-    $('html, body').animate({scrollTop:0}, 1500);
+    $("#back-to-top a").click(function(event) {
+      $('html, body').animate({scrollTop:0}, 1500);
+
+      // This prevents the default action of the anchor tag to be called.
+      //  Can be interchanged with event.preventDefault()
+      return false;
+
+    });
   });
-});
 });
