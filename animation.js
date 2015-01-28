d63c3acf89aa686bea2a1302e99ac8e900eae059
Steven <steven.github@gmail.com>
2015-01-27 19:55:21 -0800

    added home button

---
+++ animation.js
@@ -0,0 +1,23 @@
+// function scrollToAnchor(aid){
+//     var aTag = $("a[name='"+ aid +"']");
+//     $('html,body').animate({scrollTop: aTag.offset().top},'slow');
+// }
+
+$(document).ready(function() {
+  // hide #back-top first
+  $("#back-to-top").hide();
+
+  $(function () {
+      $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+          $('#back-to-top').fadeIn();
+        } else {
+          $('#back-to-top').fadeOut();
+        }
+      });
+
+  $("#back-to-top a").click(function(event) {
+    $('html, body').animate({scrollTop:0}, 1500);
+  });
+});
+});
