6a8fbe8486ab9c4ab1075a71300d6b4f52a0d403
Steven <steven.github@gmail.com>
2015-02-01 11:16:23 -0800

    re-added hide home button on startup

--- animation.js
+++ animation.js
@@ -1,39 +1,40 @@
 $(document).ready(function() {
+  // hide #back-top first
+  $("#back-to-top").hide();
 
   // Back-to-top button scrolling animation javascript
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 100) {
+      $('#back-to-top').fadeIn();
+    } else {
+      $('#back-to-top').fadeOut();
+    }
+  });
 
-    $(window).scroll(function () {
-      if ($(this).scrollTop() > 100) {
-        $('#back-to-top').fadeIn();
-      } else {
-        $('#back-to-top').fadeOut();
-      }
-    });
+  $("#back-to-top a").click(function(event) {
 
-    $("#back-to-top a").click(function(event) {
+    $('html, body').stop().animate({scrollTop:0}, 500);
 
-      $('html, body').stop().animate({scrollTop:0}, 500);
-
-      // This prevents the default action of the anchor tag to be called.
-      //  Can be interchanged with event.preventDefault()
-      return false;
-    });
+    // This prevents the default action of the anchor tag to be called.
+    //  Can be interchanged with event.preventDefault()
+    return false;
+  });
 
 
-    // Navbar scrolling animation javascript
-    $("#navbar a").click(function(event) {
+  // Navbar scrolling animation javascript
+  $("#navbar a").click(function(event) {
 
-      // else do the scroll animation
-      var link = $(event.target).attr("href");
-      var jumpToTag = $(link);
+    // else do the scroll animation
+    var link = $(event.target).attr("href");
+    var jumpToTag = $(link);
 
-      $('html,body').stop().animate({scrollTop: jumpToTag.offset().top}, 500);
-      return false;
-    });
+    $('html,body').stop().animate({scrollTop: jumpToTag.offset().top}, 500);
+    return false;
+  });
 
-    $('#fullpage').fullpage({
-      loopTop: true,
-      loopBottom: true,
-      scrollBar: true
-    });
+  $('#fullpage').fullpage({
+    loopTop: true,
+    loopBottom: true,
+    scrollBar: true
+  });
 });
