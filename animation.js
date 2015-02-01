c3592988ea8f3f0d1da418a9934d199bfff73fe1
Steven <steven.github@gmail.com>
2015-02-01 10:41:31 -0800

    added section scrolling with fullpage js

--- animation.js
+++ animation.js
@@ -1,8 +1,7 @@
 $(document).ready(function() {
-  // hide #back-top first
-  $("#back-to-top").hide();
 
-  $(function () {
+  // Back-to-top button scrolling animation javascript
+
     $(window).scroll(function () {
       if ($(this).scrollTop() > 100) {
         $('#back-to-top').fadeIn();
@@ -19,9 +18,10 @@ $(document).ready(function() {
       // This prevents the default action of the anchor tag to be called.
       //  Can be interchanged with event.preventDefault()
       return false;
-
     });
 
+
+    // Navbar scrolling animation javascript
     $("#navbar a").click(function(event) {
       stopScroll();
 
@@ -32,18 +32,24 @@ $(document).ready(function() {
       $('html,body').animate({scrollTop: jumpToTag.offset().top}, 800, continueScroll);
       return false;
     });
-  });
+
+    $('#fullpage').fullpage({
+      loopTop: true,
+      loopBottom: true,
+      scrollBar: true
+    });
+
 });
 
 // Helper Functions
 // Taken from http://stackoverflow.com/a/18445654
 function stopScroll() {
   // if user scrolls while animation, cancel the animation
-  $("html, body").bind("scroll mousedown DOMMouseScroll mousewheel keyup", function(){
+  $("html, body").bind("scroll mousedown wheel keyup", function(){
     $('html, body').stop();
   });
 }
 
 function continueScroll() {
-  $("html, body").unbind("scroll mousedown DOMMouseScroll mousewheel keyup");
+  $("html, body").unbind("scroll mousedown wheel keyup");
 }
